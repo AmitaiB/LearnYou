02b9c9073f6b7fd4192904fb0d64d93aba240d09
AmitaiB <blickstein@gmail.com>
2015-07-23 01:52:11 -0400

    adds Xcode project

---
+++ LearnYou/LearnYou/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  LearnYou
+//
+//  Created by Amitai Blickstein on 7/23/15.
+//  Copyright (c) 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
